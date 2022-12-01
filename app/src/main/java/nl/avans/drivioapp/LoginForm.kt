1de4334c33c71022e37e183d2784a4acbad03a01
Leon <leonvsc@users.noreply.github.com>
2022-12-01 12:31:11 +0100

    Feat: Made a start of the login and registration navigation

--- app/src/main/java/nl/avans/drivioapp/LoginForm.kt
+++ app/src/main/java/nl/avans/drivioapp/LoginForm.kt
@@ -2,8 +2,6 @@ package nl.avans.drivioapp
 
 import androidx.fragment.app.Fragment
 
-class LoginFragment : Fragment(R.layout.fragment_login) {
-
-    
+class LoginForm : Fragment(R.layout.fragment_login_form) {
 
 }
--- app/src/main/java/nl/avans/drivioapp/RegisterForm.kt
+++ app/src/main/java/nl/avans/drivioapp/RegisterForm.kt
@@ -2,8 +2,6 @@ package nl.avans.drivioapp
 
 import androidx.fragment.app.Fragment
 
-class LoginFragment : Fragment(R.layout.fragment_login) {
-
-    
+class RegisterForm : Fragment(R.layout.fragment_register_form) {
 
 }
