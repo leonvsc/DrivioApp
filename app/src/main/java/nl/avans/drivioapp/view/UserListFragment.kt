93630d8ffe0a84e6c00f870b57d53622d43fc87e
Huseyin Tufekci <h.tufekci@student.avans.nl>
2023-01-03 14:38:57 +0100

    recyclerView Added to display the userlist

--- app/src/main/java/nl/avans/drivioapp/view/UserListFragment.kt
+++ app/src/main/java/nl/avans/drivioapp/view/UserListFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import nl.avans.drivioapp.databinding.FragmentUserListBinding
+import nl.avans.drivioapp.view.adapter.UserListAdapter
+import nl.avans.drivioapp.viewModel.UserViewModel
 
 class UserListFragment : Fragment() {
 
     private var _binding: FragmentUserListBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var userViewModel: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +26,17 @@ class UserListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentUserListBinding.inflate(inflater, container, false)
+
+        val adapter = UserListAdapter()
+        val recyclerView = binding.userListRecyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        userViewModel.getAllUsers.observe(viewLifecycleOwner, Observer { userList ->   // userList was it, heb get gewoon veranderd, vergeet niet dit is eigenlijk de query die we hadden binnen de userDao
+            adapter.setData(userList)
+        })
+
         return binding.root
     }
 
--- app/src/main/java/nl/avans/drivioapp/viewModel/UserViewModel.kt
+++ app/src/main/java/nl/avans/drivioapp/viewModel/UserViewModel.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
-
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import nl.avans.drivioapp.database.UserDatabase
@@ -14,7 +13,7 @@ import nl.avans.drivioapp.model.User
 // Let op! deze viewmodel is anders dan een gewone viewmodel want het bevat een application reference
 class UserViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val getAllUsers: LiveData<List<User>>
+    val getAllUsers: LiveData<List<User>>
     private val repository: UserRepository
 
     // always first executed when UserViewModel is called!
