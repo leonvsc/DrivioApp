f6002605596c59df30b0fd9bb659ed67135fcc87
Niels Warnaar <76254458+NielsWarnaar@users.noreply.github.com>
2023-01-12 13:08:22 +0100

    Started with image upload from galery and camera integration

--- app/src/main/java/nl/avans/drivioapp/AddElectricCarFragment.kt
+++ app/src/main/java/nl/avans/drivioapp/AddElectricCarFragment.kt
@@ -1,22 +1,75 @@
 package nl.avans.drivioapp
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.appsearch.SetSchemaRequest.READ_EXTERNAL_STORAGE
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Base64
+import android.util.Base64.encodeToString
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
+import androidx.core.content.PermissionChecker.checkSelfPermission
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import aws.sdk.kotlin.services.s3.S3Client
+import aws.sdk.kotlin.services.s3.model.PutObjectRequest
+import aws.smithy.kotlin.runtime.content.asByteStream
 import nl.avans.drivioapp.databinding.FragmentAddElectricCarBinding
 import nl.avans.drivioapp.model.ElectricCar
 import nl.avans.drivioapp.model.User
 import nl.avans.drivioapp.viewModel.AddElectricCarViewModel
+import java.io.ByteArrayOutputStream
+import java.io.File
 
 
 class AddElectricCarFragment : Fragment(R.layout.fragment_add_electric_car) {
     private val addElectricCarViewModel: AddElectricCarViewModel by viewModels();
     private var _binding: FragmentAddElectricCarBinding? = null;
     private val binding get() = _binding!!;
+
+    private val REQUEST_CODE = 100
+    private val REQUEST_IMAGE_CAPTURE = 1
+
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+    }
+
+    private fun openGalleryForImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_CODE)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE){
+            println("Image uploaded")
+            val imageUri: Uri? = data?.data;
+            binding.ivUploadedImage.setImageURI(imageUri)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +82,14 @@ class AddElectricCarFragment : Fragment(R.layout.fragment_add_electric_car) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState);
+
+        binding.addPhotoBtn.setOnClickListener {
+            openGalleryForImage()
+        }
+
+        binding.cameraBtn.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
 
         binding.postElectricCarBtn.setOnClickListener {
 
---
+++ app/src/main/java/nl/avans/drivioapp/ImagesS3AWS.kt
@@ -0,0 +1,28 @@
+package nl.avans.drivioapp
+
+import aws.sdk.kotlin.services.s3.*
+import aws.sdk.kotlin.services.s3.model.PutObjectRequest
+import aws.smithy.kotlin.runtime.content.asByteStream
+import java.io.File
+import kotlin.system.exitProcess
+
+class ImagesS3AWS {
+
+    suspend fun putS3Object(bucketName: String, objectKey: String, objectPath: String) {
+
+        val metadataVal = mutableMapOf<String, String>()
+        metadataVal["myVal"] = "test"
+
+        val request = PutObjectRequest {
+            bucket = bucketName
+            key = objectKey
+            metadata = metadataVal
+            body = File(objectPath).asByteStream()
+        }
+
+        S3Client { region = "eu-west-1" }.use { s3 ->
+            val response = s3.putObject(request)
+            println("Tag information is ${response.eTag}")
+        }
+    }
+}
