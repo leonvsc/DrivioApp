2cbbda9cabf3a1235f9653a9d9f4965160f43f07
Niels Warnaar <76254458+NielsWarnaar@users.noreply.github.com>
2023-01-14 19:01:54 +0100

    Small change

--- app/src/main/java/nl/avans/drivioapp/AWS/s3-constants.kt
+++ app/src/main/java/nl/avans/drivioapp/AWS/s3-constants.kt
@@ -1,8 +1,6 @@
 package nl.avans.drivioapp.AWS
 
 object `s3-constants` {
-    val ACCESS_ID:String = "AKIASSVPQYFK57XYNOAL"
-    val SECRET_KEY:String = "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"
     val BUCKET_NAME: String = "images-drivio-app"
     val AWS_REGION: String = "eu-west-1"
 }
