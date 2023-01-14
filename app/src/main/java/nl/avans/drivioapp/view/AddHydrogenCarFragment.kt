77b9c1aa4beab5b72af1caf30e9d8c83986198d5
Leon <leonvsc@users.noreply.github.com>
2023-01-14 14:09:36 +0100

    Feat: Added coordinate generator to FuelCar and HydrogenCar

--- app/src/main/java/nl/avans/drivioapp/view/AddHydrogenCarFragment.kt
+++ app/src/main/java/nl/avans/drivioapp/view/AddHydrogenCarFragment.kt
@@ -12,6 +12,7 @@ import nl.avans.drivioapp.databinding.FragmentAddHydrogenCarBinding
 import nl.avans.drivioapp.model.HydrogenCar
 import nl.avans.drivioapp.model.User1
 import nl.avans.drivioapp.viewModel.AddHydrogenCarViewModel
+import kotlin.random.Random
 
 class AddHydrogenCarFragment : Fragment(R.layout.fragment_add_hydrogen_car) {
     private val addHydrogenCarViewModel: AddHydrogenCarViewModel by viewModels();
@@ -43,6 +44,8 @@ class AddHydrogenCarFragment : Fragment(R.layout.fragment_add_hydrogen_car) {
             val tankSize = binding.etTankSize.text.toString().toInt()
             val gearBox = binding.etGearBox.text.toString()
             val brand = binding.etBrand.text.toString()
+            val randomLatitude = Random.nextDouble(52.2, 52.4)
+            val randomLongitude = Random.nextDouble(4.7, 4.9)
 
             val hydrogenCar = HydrogenCar(
                 carRange,
@@ -57,8 +60,8 @@ class AddHydrogenCarFragment : Fragment(R.layout.fragment_add_hydrogen_car) {
                 gearBox,
                 brand,
                 User1(23),
-                0.0,
-                0.0
+                randomLatitude,
+                randomLongitude
             )
             addHydrogenCarViewModel.postHydrogenCarWithResponse(hydrogenCar);
 
