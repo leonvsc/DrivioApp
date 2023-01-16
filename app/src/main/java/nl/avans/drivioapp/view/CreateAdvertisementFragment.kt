f90a737d190046b606e5a985d6ed55f7225aedfb
Niels Warnaar <76254458+NielsWarnaar@users.noreply.github.com>
2023-01-16 20:13:42 +0100

    Added code for the spinner

--- app/src/main/java/nl/avans/drivioapp/view/CreateAdvertisementFragment.kt
+++ app/src/main/java/nl/avans/drivioapp/view/CreateAdvertisementFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -13,13 +15,16 @@ import nl.avans.drivioapp.model.Advertisement
 import nl.avans.drivioapp.model.ElectricCar
 import nl.avans.drivioapp.model.User1
 import nl.avans.drivioapp.viewModel.AdvertisementViewModel
+import nl.avans.drivioapp.viewModel.MyCarsViewModel
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 class CreateAdvertisementFragment : Fragment(R.layout.fragment_create_advertisement) {
     private var _binding: FragmentCreateAdvertisementBinding? = null;
     private val binding get() = _binding!!;
     private val advertisementViewModel: AdvertisementViewModel by viewModels()
+    private val myCarsViewModel: MyCarsViewModel by viewModels()
     private lateinit var startDate: String
     private lateinit var endDate: String
 
@@ -54,35 +59,59 @@ class CreateAdvertisementFragment : Fragment(R.layout.fragment_create_advertisem
             binding.tvEndDate.text = "Enddate: $endDate"
         }
 
-        binding.btnConfirm.setOnClickListener {
-            val title = binding.etTitle.text.toString()
-            val description = binding.etDescription.text.toString()
-            val price = binding.etPrice.text.toString().toDouble()
-            val startDate = startDate
-            val endDate = endDate
-            val carId = 55
+        val carsList = arrayListOf<String?>()
 
-            val advertisement = Advertisement(
-                null,
-                title,
-                description,
-                price,
-                startDate,
-                endDate,
-                User1(47),
-                ElectricCar(carId)
-            )
-            advertisementViewModel.postAdvertisementWithResponse(advertisement)
+        myCarsViewModel.electricCarResponse.observe(viewLifecycleOwner) {
+            // Add car location markers on the map
+            var myCars = myCarsViewModel.electricCarResponse.value!!
+            myCars = myCars.filter { it.user?.userId == 47 }
+            for (myCar in myCars) {
+                carsList.add(myCar.carId.toString())
+            }
 
-            advertisementViewModel.postAdvertisementResponse.observe(viewLifecycleOwner) {
-                val response = advertisementViewModel.postAdvertisementResponse.value
+            val spinner: Spinner = binding.spinnerLanguages
 
-                if (response?.code() == 200) {
-                    Toast.makeText(activity, "Success!!", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(activity, "Failed!!", Toast.LENGTH_SHORT).show()
+            val adapter: ArrayAdapter<String> =
+                ArrayAdapter<String>(
+                    this.requireContext(),
+                    android.R.layout.simple_spinner_item,
+                    carsList
+                )
+
+
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+            spinner.adapter = adapter;
+
+            binding.btnConfirm.setOnClickListener {
+                val title = binding.etTitle.text.toString()
+                val description = binding.etDescription.text.toString()
+                val price = binding.etPrice.text.toString().toDouble()
+                val startDate = startDate
+                val endDate = endDate
+                val carId = 55
+
+                val advertisement = Advertisement(
+                    null,
+                    title,
+                    description,
+                    price,
+                    startDate,
+                    endDate,
+                    User1(47),
+                    ElectricCar(carId)
+                )
+                advertisementViewModel.postAdvertisementWithResponse(advertisement)
+
+                advertisementViewModel.postAdvertisementResponse.observe(viewLifecycleOwner) {
+                    val response = advertisementViewModel.postAdvertisementResponse.value
+
+                    if (response?.code() == 200) {
+                        Toast.makeText(activity, "Success!!", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(activity, "Failed!!", Toast.LENGTH_SHORT).show()
+                    }
+
                 }
-
             }
         }
     }
